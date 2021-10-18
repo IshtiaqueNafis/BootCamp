f077d0d277af14af8752df423346cde118d288d2
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-10-18 15:10:55 -0400

    added aggerate function  on course model to calculate avrage cost for bootcampcourse

--- models/CourseModel.js
+++ models/CourseModel.js
@@ -20,7 +20,7 @@ const CourseSchema = new mongoose.Schema({
     minimumSkill: {
         type: String,
         required: [true, "Please add a minimum skill"],
-        enum: ["beginner","intermediate","advanced"]
+        enum: ["beginner", "intermediate", "advanced"]
     },
     ScholarshipAvailable: {
         type: String,
@@ -39,4 +39,54 @@ const CourseSchema = new mongoose.Schema({
 
 
 });
+
+//region static method to get course tuition
+//statics means it will run on courseSchema
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log(`Calculating average cost`);
+    const obj = await this.aggregate([
+        // this is a pipie line so do the avrage cost based on bootcamp Id
+        {
+            $match: {bootcamp: bootcampId} // use match to find the bootcamp Id.
+
+        },
+        //first find the bootcamp with id
+        {
+            $group: {
+                _id: '$bootcamp', // group by id $must be given here for it to work // get the id of the bootcamp foregin key
+                averageCost: {$avg: '$tuition'}  // then get the avrage ost based on tutiton property
+            }
+        }
+    ]);
+
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        });
+        // by using this.model will allow to use the Bootcamp model on BootcampModel Schema
+        // $match: {bootcamp: bootcampId} --> means this will refer to the bootcamp from the model
+        // obj[0].averageCost --> cause returns a 0 object in array
+
+    } catch (e) {
+        console.log(e);
+    }
+
+
+}
+//endregion
+
+//call average cost after save // this is post cause save is calculated after update
+CourseSchema.post('save', function () {
+
+    this.constructor.getAverageCost(this.bootcamp) //$match: {bootcamp: bootcampId} bootcamp is coming from here
+})
+
+//remove calculation after removing item.
+CourseSchema.pre('remove', function () {
+
+    this.constructor.getAverageCost(this.bootcamp) ////$match: {bootcamp: bootcampId} bootcamp is coming from here
+})
+
+
 module.exports = mongoose.model('Course', CourseSchema);
